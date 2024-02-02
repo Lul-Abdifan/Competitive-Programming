c0abadfad11c40da8e3b4206b71eddd9d28f6378
Abdi Bekele <86189460+Lul-Abdifan@users.noreply.github.com>
2024-02-02 12:01:24 +0300

    Create power_module.py

---
+++ power_module.py
@@ -0,0 +1,7 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+def power_module(a,b,m):
+    print(pow(a,b))
+    print(pow(a,b,m))
+if __name__ == "__main__":
+    power_module(int(input()),int(input()),int(input()))
+    
