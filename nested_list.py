c673d64106491c9d1a2152df0e25f8e9a67d4c9a
Abdi Bekele <86189460+Lul-Abdifan@users.noreply.github.com>
2024-02-08 18:58:50 +0300

    Update nested_list.py

--- nested_list.py
+++ nested_list.py
@@ -1,16 +1,16 @@
 if __name__ == '__main__':
     lists = []
     
-    # append the element in the lists
+    # append the element to the lists
     for _ in range(int(input())):
         name = input()
         score = float(input())
         lists.append([name,score])
     
-    # find the second score of the list
+    # find the second-lowest score on the list
     second_lowest_no = sorted(set(lists[i][1] for i in range(len(lists))))[1]
     
-    # append the array of the second score to res array
+    # append the array of the second-lowest score to the result array
     result = [name for name,score in lists if score == second_lowest_no]
     result.sort()
     
